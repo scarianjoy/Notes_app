4cdad84184b54e5aa14dbf3090675b3655405e8c
Scaria <scaria1njoy99@gmail.com>
2020-07-10 16:22:26 +0530

    basic frame of notesapp

--- notes_list_app/views.py
+++ notes_list_app/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import List
+from .forms import Listform
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html',{})
+    if request.method == 'POST':
+        form = Listform(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all 
+            messages.success(request, ('Notes have been added to the list'))
+            return render(request, 'home.html', {'all_items': all_items})
+    
+    else:
+            all_items = List.objects.all 
+            return render(request, 'home.html', {'all_items': all_items})
