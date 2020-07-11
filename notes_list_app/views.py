14f7f8377f2c7604593fecc001b1909a9bc1e1ca
Scaria <scaria1njoy99@gmail.com>
2020-07-11 13:35:34 +0530

    basic notes app v1

--- notes_list_app/views.py
+++ notes_list_app/views.py
@@ -14,7 +14,46 @@ def home(request):
             all_items = List.objects.all 
             messages.success(request, ('Notes have been added to the list'))
             return render(request, 'home.html', {'all_items': all_items})
-    
+        else:
+            all_items = List.objects.all 
+            return render(request, 'home.html', {'all_items': all_items})
     else:
             all_items = List.objects.all 
             return render(request, 'home.html', {'all_items': all_items})
+
+
+def delete(request, list_id):
+    item = List.objects.get(pk = list_id)
+    item.delete()
+    messages.success(request, ('Item has been deleted'))
+    return redirect('home')
+
+
+def edit(request, list_id):
+    if request.method == 'POST':
+        item = List.objects.get(pk=list_id)
+
+        form = Listform(request.POST or None, instance = item)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Item Has Been Edited!'))
+            return redirect('home')
+
+    else:
+        item = List.objects.get(pk=list_id)
+        return render(request, 'edit.html', {'item': item})
+
+
+# def cross_off(request, list_id):
+#     item  = List.objects.get(pk=list_id)
+#     item.completed = True
+#     item.save()
+#     return redirect('home')
+
+
+# def uncross(request, list_id):
+#     item  = List.objects.get(pk=list_id)
+#     item.completed = False
+#     item.save()
+#     return redirect('home')
